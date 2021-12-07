56670ab8f06d2cc2c5767c36b8c4a2333840354a
GaryNLOL <dev.garynlol@gmail.com>
2021-12-07 07:36:23 -0500

    Changed file structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# Exercise-Reminder
-App to remind you to make exercise after sitting for a lot of time in front of your computer.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
-#include "../include/EasyWindow/EasyWindow.hpp"
-#include "../include/App.hpp"
 #include <SFML/Graphics.hpp>
-#include <list>
+#include <ctime>
 
-int main(int argc, char* argv[])
+int main()
 {
-    App app(argc == 1 ? RunType::Manual:RunType::Scheduled);
-    return app.run();
+    sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().height), "Let's do exercise!", sf::Style::None);
+    int resting_time = 600;
+    std::time_t last_time = std::time(0);
+    sf::Texture texture;
+    if(!texture.loadFromFile("./assets/exercise.jpg"))
+        throw;
+    sf::Sprite sprite(texture);
+    sprite.setScale(sf::VideoMode::getDesktopMode().width/sprite.getGlobalBounds().width,sf::VideoMode::getDesktopMode().height/sprite.getGlobalBounds().height);
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event));
+        window.clear();
+        window.draw(sprite);
+        window.display();
+        std::time_t time_now = std::time(0);
+        std::time_t elapsed_time = time_now - last_time;
+        last_time = time_now;
+        resting_time -= elapsed_time;
+        if(resting_time <= 0)
+            window.close();
+    }
 }
